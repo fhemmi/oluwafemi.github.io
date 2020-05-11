c01ecbd430deb90a12e458b12a21bb54219ee0c3
Bodunde Eben Oluwafemi <bodundeoluwafemi00@gmail.com>
2020-05-11 17:22:15 +0100

    second commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// SLIDEDOWN FUNCTION
+//
+// $(document).ready(function(){
+//   $("#slide").ready(function(){
+//     $("#slideDown").slideDown("slow");
+//     console.log("js is workimg");
+//   });
+// });
